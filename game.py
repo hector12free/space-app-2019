0c78f80597e18bdcdab7d1de03204c7293097e13
Tina Huang <t42huang@gmail.com>
2019-10-20 15:59:08 -0400

    refractor code

--- game.py
+++ game.py
@@ -14,10 +14,8 @@ from button import button
 # Set isDemoMode flag to True to only play with level 2, which is more interesting
 isDemoMode = True
 
-# level 1 - basic; level 2 - with collector
-level = 1
-if isDemoMode:
-    level = 2 # TODO change it back
+# level 1 - basic; level 2 - with collector to clean space junk
+level = 2 if isDemoMode else 1
 
 pygame.init()
 pygame.display.set_caption("Space Junk Terminator")
@@ -27,40 +25,40 @@ width = 800
 height = 600
 
 # Colors
-red = (197, 38, 32) # (255,0,0)
+red = (197, 38, 32) # (255, 0, 0)
 redDark = (154, 30, 25)
-blue = (0,0,255)
-yellow = (255,255,0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
 green = (36, 194, 179) # (0, 200, 0)
 greenDark = (28, 151, 139)
-background_color = (0,0,0)
+backgroundColor = (0, 0, 0)
 
 # Starship collector
-collector_size = 50
-collector_pos = [width/3, height-2*collector_size]
-collectorImg_filepath = 'images/LogoMakr_0rPhIj.png' 
-collectorImg = pygame.image.load(collectorImg_filepath)
+collectorSize = 50
+collectorPos = [width / 3, height - 2 * collectorSize]
+collectorImgFilePath = 'images/LogoMakr_0rPhIj.png' 
+collectorImg = pygame.image.load(collectorImgFilePath)
 collectorImg = pygame.transform.rotozoom(collectorImg, 0, 0.125)
 
 # Satellite
-satellite_size = 50
-satellite_pos = [width/2, height-2*collector_size]
-satelliteImg_filepath = 'images/satellite.png' 
-satelliteImg = pygame.image.load(satelliteImg_filepath)
+satelliteSize = 50
+satellitePos = [width / 2, height - 2 * collectorSize]
+satelliteImgFilePath = 'images/satellite.png' 
+satelliteImg = pygame.image.load(satelliteImgFilePath)
 satelliteImg = pygame.transform.rotozoom(satelliteImg, 0, 0.125)
 
 # Enemy - Space Junk
-enemy_size = 50
-enemy_pos = [random.randint(0,width-enemy_size), 0]
-enemy_list=[enemy_pos]
-spaceJunkImg_filepath = 'images/spaceJunk1.png'
-spaceJunkImg = pygame.image.load(spaceJunkImg_filepath)
+enemySize = 50
+enemyPos = [random.randint(0, width - enemySize), 0]
+enemyList = [enemyPos]
+spaceJunkImgFilePath = 'images/spaceJunk1.png'
+spaceJunkImg = pygame.image.load(spaceJunkImgFilePath)
 spaceJunkImg = pygame.transform.rotozoom(spaceJunkImg, 0, 0.03)
 
 speed = 20
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width, height))
 
-game_over = False
+gameOver = False
 
 score = 0
 
@@ -68,23 +66,23 @@ clock = pygame.time.Clock()
 
 myfont = pygame.font.SysFont("monospace", 35)
 
-def initializeStates(collector_pos, satellite_pos, enemy_list):
-    collector_pos = [width/3, height-2*collector_size]
-    satellite_pos = [width/2, height-2*collector_size]
+def initializeStates(collectorPos, satellitePos, enemyList):
+    collectorPos = [width / 3, height - 2 * collectorSize]
+    satellitePos = [width / 2, height - 2 * collectorSize]
 
-    enemy_pos = [random.randint(0,width-enemy_size), 0]
-    enemy_list=[enemy_pos]
+    enemyPos = [random.randint(0, width - enemySize), 0]
+    enemyList = [enemyPos]
 
-def drawCollector(x,y):
-    screen.blit(collectorImg,(x,y))
+def drawCollector(x, y):
+    screen.blit(collectorImg, (x, y))
 
-def drawSpaceJunk(x,y):
-    screen.blit(spaceJunkImg,(x,y))
+def drawSpaceJunk(x, y):
+    screen.blit(spaceJunkImg, (x, y))
 
-def drawSatellite(x,y):
-    screen.blit(satelliteImg,(x,y))
+def drawSatellite(x, y):
+    screen.blit(satelliteImg, (x, y))
 
-def set_speed(score,speed):
+def setSpeed(score, speed):
     # if score < 20:
     #     speed = 20
     # elif score < 40:
@@ -94,62 +92,62 @@ def set_speed(score,speed):
     # elif score < 80:
     #     speed = 40
     # else:
-    #     speed = speed = score/2+1
+    #     speed = speed = score / 2 + 1
     return speed
 
-def drop_enemies(enemy_list):
+def dropEnemies(enemyList):
     delay = random.random()
-    if len(enemy_list) < 10 and delay < 0.1:
-        x_pos = random.randint(0,width-enemy_size)
-        y_pos = 0
-        enemy_list.append([x_pos, y_pos])
+    if len(enemyList) < 10 and delay < 0.1:
+        posX = random.randint(0, width - enemySize)
+        posY = 0
+        enemyList.append([posX, posY])
 
-def draw_enemies(enemy_list):
-    for enemy_pos in enemy_list:
-        drawSpaceJunk(enemy_pos[0], enemy_pos[1])
+def drawEnemies(enemyList):
+    for enemyPos in enemyList:
+        drawSpaceJunk(enemyPos[0], enemyPos[1])
 
 # update the position of the enemy
-def update_enemey_positions(enemy_list, score):
-    for idx, enemy_pos in enumerate(enemy_list):
-        if enemy_pos[1] >=0 and enemy_pos[1] < height:
-            enemy_pos[1] += speed
+def updateEnemeyPositions(enemyList, score):
+    for idx, enemyPos in enumerate(enemyList):
+        if enemyPos[1] >=0 and enemyPos[1] < height:
+            enemyPos[1] += speed
         else:
-            enemy_list.pop(idx)
+            enemyList.pop(idx)
             score += 1
     return score
 
-def cleanJunk(enemy_list):
-    for enemy_pos in enemy_list:
-        if detect_collision(enemy_pos, collector_pos, collector_size):
-            enemy_list.remove(enemy_pos)
+def cleanJunk(enemyList):
+    for enemyPos in enemyList:
+        if detectCollision(enemyPos, collectorPos, collectorSize):
+            enemyList.remove(enemyPos)
     
-def hasCollisionWithSatellite(enemy_list):
-    for enemy_pos in enemy_list:
-        if detect_collision(enemy_pos, satellite_pos, satellite_size):
+def hasCollisionWithSatellite(enemyList):
+    for enemyPos in enemyList:
+        if detectCollision(enemyPos, satellitePos, satelliteSize):
             return True
     return False
 
-def detect_collision(object_pos, enemy_pos, object_size):
-    p_x = object_pos[0]
-    p_y = object_pos[1]
+def detectCollision(objectPos, enemyPos, objectSize):
+    p_x = objectPos[0]
+    p_y = objectPos[1]
 
-    e_x= enemy_pos[0]
-    e_y=enemy_pos[1]
+    e_x= enemyPos[0]
+    e_y=enemyPos[1]
 
-    if (e_x >= p_x and e_x < (p_x + object_size)) or (p_x >= e_x and p_x < (e_x+enemy_size)):
-        if (e_y >= p_y and e_y < (p_y + object_size)) or (p_y >= e_y and p_y < (e_y+enemy_size)):
+    if (e_x >= p_x and e_x < (p_x + objectSize)) or (p_x >= e_x and p_x < (e_x + enemySize)):
+        if (e_y >= p_y and e_y < (p_y + objectSize)) or (p_y >= e_y and p_y < (e_y + enemySize)):
             return True
     return False
 
 def redrawWindow(continueBtn, quitBtn):
-    screen.fill(background_color)
-    continueBtn.draw(screen, (0,0,0))
-    quitBtn.draw(screen, (0,0,0))
+    screen.fill(backgroundColor)
+    continueBtn.draw(screen, (0, 0, 0))
+    quitBtn.draw(screen, (0, 0, 0))
 
 def drawTransitionPage(continueBtnText):
     stayAtThisPage = True
-    continueBtn = button(green, width/3, height*2/3, 120, 50, continueBtnText)
-    quitBtn = button(red, width*2/3, height*2/3, 120, 50, 'Close')
+    continueBtn = button(green, width / 3, height * 2 / 3, 120, 50, continueBtnText)
+    quitBtn = button(red, width * 2 / 3, height * 2 / 3, 120, 50, 'Close')
 
     while stayAtThisPage:
         redrawWindow(continueBtn, quitBtn)
@@ -183,48 +181,48 @@ def drawTransitionPage(continueBtnText):
 drawTransitionPage("Start")
 
 while True:
-    while not game_over:
+    while not gameOver:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
             if event.type == pygame.KEYDOWN:
-                x = collector_pos[0]
-                y = collector_pos[1]
+                x = collectorPos[0]
+                y = collectorPos[1]
 
                 if event.key == pygame.K_LEFT and x > speed:
-                    x -= collector_size
-                elif event.key == pygame.K_RIGHT and x < width - speed - collector_size:
-                    x += collector_size
+                    x -= collectorSize
+                elif event.key == pygame.K_RIGHT and x < width - speed - collectorSize:
+                    x += collectorSize
                 elif event.key == pygame.K_UP and y > speed:
-                    y -= collector_size
-                elif event.key == pygame.K_DOWN and y < height - speed - collector_size:
-                    y += collector_size
+                    y -= collectorSize
+                elif event.key == pygame.K_DOWN and y < height - speed - collectorSize:
+                    y += collectorSize
 
-                collector_pos = [x,y] 
+                collectorPos = [x,y] 
 
-        screen.fill(background_color)
+        screen.fill(backgroundColor)
 
-        drop_enemies(enemy_list)
-        score = update_enemey_positions(enemy_list, score)
-        speed = set_speed(score, speed)
+        dropEnemies(enemyList)
+        score = updateEnemeyPositions(enemyList, score)
+        speed = setSpeed(score, speed)
 
-        text = "Score:" + str(score)
+        text = "Score: " + str(score)
         label = myfont.render(text, 1, yellow)
-        screen.blit(label,(width-200,height-40))
+        screen.blit(label, (width - 200, height - 40))
 
         if level > 1: # only clear junk when level > 1
-            cleanJunk(enemy_list)
+            cleanJunk(enemyList)
 
-        if not isDemoMode and hasCollisionWithSatellite(enemy_list):
-            game_over=True
+        if not isDemoMode and hasCollisionWithSatellite(enemyList):
+            gameOver=True
             break
 
-        draw_enemies(enemy_list)
+        drawEnemies(enemyList)
 
-        drawSatellite(satellite_pos[0], satellite_pos[1])
+        drawSatellite(satellitePos[0], satellitePos[1])
         if (level > 1):
-            drawCollector(collector_pos[0], collector_pos[1])
+            drawCollector(collectorPos[0], collectorPos[1])
 
         clock.tick(30)
 
@@ -234,8 +232,8 @@ while True:
     drawTransitionPage("Next")
 
     # when user select Next button, user go to play next level
-    game_over = False
+    gameOver = False
     level += 1
     # score = 0
-    initializeStates(collector_pos, satellite_pos, enemy_list)
+    initializeStates(collectorPos, satellitePos, enemyList)
     print("next level: ", level)
